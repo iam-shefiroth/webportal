ddadb9b9b0f9ed226b37fe403a6d1bdbb850dcfa
radical rogical <radical rogical@DESKTOP-RN8S4UD>
2021-06-25 11:42:10 +0900

    最初のコミットです。

---
+++ src/main/java/jp/ac/hcs/s3a315/WebportalFwApplication.java
@@ -0,0 +1,13 @@
+package jp.ac.hcs.s3a315;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebportalFwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebportalFwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/ac/hcs/s3a315/WebportalFwApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.ac.hcs.s3a315;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebportalFwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
