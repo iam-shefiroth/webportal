3a8d1a74edb3aef117e91deccb3de56b13b05cdc
radical rogical <radical rogical@DESKTOP-RN8S4UD>
2021-06-30 10:36:02 +0900

    未完成

--- src/main/java/jp/ac/hcs/s3a315/weather/WeatherController.java
+++ src/main/java/jp/ac/hcs/s3a315/weather/WeatherController.java
@@ -1,5 +1,36 @@
 package jp.ac.hcs.s3a315.weather;
 
-public class WeatherController {
+import java.security.Principal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class WeatherController{
+
+	@Autowired
+	private WeatherService weatherService;
+	/**
+	 * 
+	 * @param weather 選択した地域（初期は札幌、後に全地域対応可能）
+	 * @param principal ログイン情報
+	 * @param model
+	 * @return 検索結果-天気予報
+	 */
+	
+	@PostMapping("/weather")
+	public String getWeather(@RequestParam("weather") String weather,
+			Principal principal,Model model) {
+		WeatherEntity weatherEntity = weatherService.getweather(weather);
+		model.addAttribute("weatherEntity",weatherEntity);
+		
+		log.info("「" + principal.getName() + "」choise area to wrather : " + weather);
+		return "weather/weather";
+	}
 }
--- src/main/java/jp/ac/hcs/s3a315/weather/WeatherService.java
+++ src/main/java/jp/ac/hcs/s3a315/weather/WeatherService.java
@@ -1,5 +1,87 @@
 package jp.ac.hcs.s3a315.weather;
 
-public class WeatherService {
+import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * 郵便番号情報を操作する
+ * zipcloud社の郵便番号検索APIを活用する
+ *- https://weather.tsukumijima.net/api/forecast?city={cityCode}
+ */
+@Transactional
+@Service
+
+public class WeatherService {
+	
+	@Autowired
+	RestTemplate restTemplate;
+	
+	/**郵便番号検索API リクエストURL*/
+	private static final String URL=
+			"https://weather.tsukumijima.net/api/forecast?city={weather}";
+	
+	/**
+	 * 指定した郵便番号に紐づく郵便番号情報を取得する
+	 * @param weather 郵便番号(7桁、ハイフン無し)
+	 * @return ZipCodeEntity
+	 */
+	public WeatherEntity getweather(String weather) {
+		
+		
+		//APIへアクセスして、結果を取得
+		String json=restTemplate.getForObject(URL, String.class,weather);
+		System.out.println(json);
+		
+		//エンティティクラスを生成
+		WeatherEntity weatherEntity = new WeatherEntity();
+		
+		//jsonクラスへの変換失敗のため、例外処理
+		try {
+			//変換クラスを生成し、文字列からjspnクラスへ変換する(例外の可能性あり)
+			ObjectMapper mapper=new ObjectMapper();
+			JsonNode node=mapper.readTree(json);
+			
+			//statusパラメータの抽出
+			String title =node.get("title").asText();
+			weatherEntity.setTitle(title);
+			//messageパラメータの抽出
+			String description=node.get("description").asText();
+			weatherEntity.setDescription(description);
+			
+			//メッセージに何か入力されたらエラー処理を行う。未定
+			if (node.get("results") == null) {
+				WeatherData wetherData=new WeatherData();
+				
+				wetherData.setDateLabel("条件に該当する住所は見つかりませんでした。");
+				
+				weatherEntity.getForecasts().add(wetherData);
+			}else {
+				//forecastsパラメータの抽出(配列分取得する)
+				for(JsonNode forecast:node.get("forecasts")) {
+					//データクラスの生成(forecasts1件分)
+					WeatherData weatherData=new WeatherData();
+					
+					weatherData.setDateLabel(forecast.get("dateLabel").asText());
+					weatherData.setTelop(forecast.get("telop").asText());
+					
+					//可変長配列の末尾に追加
+					weatherEntity.getForecasts().add(weatherData);
+				}
+			
+			
+			}
+		}catch(IOException e) {
+			//例外発生時は、エラーメッセージの詳細を標準エラー出力
+			e.printStackTrace();
+			
+		}
+		return weatherEntity;
+	}
 }
