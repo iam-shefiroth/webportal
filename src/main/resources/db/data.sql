ed994860ab918e6147744b5fde950696288d33cc
radical rogical <radical rogical@DESKTOP-RN8S4UD>
2021-07-01 14:50:02 +0900

    sa

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,6 +1,8 @@
 /* タスクテーブルのデータ */
+DELETE FROM task;
 INSERT INTO task (id, user_id, comment, limitday) VALUES (1, 'user', 'これやる', '2020-03-23');
 INSERT INTO task (id, user_id, comment, limitday) VALUES (2, 'owner', 'あれやる', '2020-03-24');
 INSERT INTO task (id, user_id, comment, limitday) VALUES (3, 'tester', 'それやる', '2020-03-31');
 INSERT INTO task (id, user_id, comment, limitday) VALUES (4, 'developer', 'どれやる', '2020-03-25');
+INSERT INTO task (id, user_id, comment, limitday) VALUES (6, 'user', 'セフィロス', '2022-03-23');
 INSERT INTO task (id, user_id, comment, limitday) VALUES (5, 'master', 'もっとやる', '2020-04-20');
