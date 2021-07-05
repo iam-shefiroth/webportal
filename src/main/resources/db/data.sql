ba04bf70b287fe71412271702b5862deb117a517
radical rogical <radical rogical@DESKTOP-RN8S4UD>
2021-07-05 22:04:42 +0900

    sdfg

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -4,5 +4,5 @@ INSERT INTO task (id, user_id, comment, limitday) VALUES (1, 'user', 'これや�
 INSERT INTO task (id, user_id, comment, limitday) VALUES (2, 'owner', 'あれやる', '2020-03-24');
 INSERT INTO task (id, user_id, comment, limitday) VALUES (3, 'tester', 'それやる', '2020-03-31');
 INSERT INTO task (id, user_id, comment, limitday) VALUES (4, 'developer', 'どれやる', '2020-03-25');
-INSERT INTO task (id, user_id, comment, limitday) VALUES (6, 'user', 'セフィロス', '2022-03-23');
+INSERT INTO task (id, user_id, comment, limitday) VALUES (6, 'user', 'セフィロスごっこをする', '2022-03-23');
 INSERT INTO task (id, user_id, comment, limitday) VALUES (5, 'master', 'もっとやる', '2020-04-20');
