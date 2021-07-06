59665335252d3fdb2fa732b6db450bc90a46b0f2
radical rogical <radical rogical@DESKTOP-RN8S4UD>
2021-07-06 12:30:46 +0900

    ｚｆｄｇ

--- src/main/java/jp/ac/hcs/s3a315/gourmet/GourmetService.java
+++ src/main/java/jp/ac/hcs/s3a315/gourmet/GourmetService.java
@@ -26,9 +26,7 @@ public class GourmetService {
 	private static final String API_KEY = "7ee5e8d9aa08642d";
 	
 	/** グルメサーチAPI リクエストURL*/
-	private static final String URL = 
-			"http://webservice.recruit.co.jp/hotpepper/gourmet/v1/search?key={API_KEY}"
-			+ "&name={keyword}&large_service_area={large_service_area}&format=json";
+	private static final String URL = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key={API_KEY}&name={shopname}&large_service_area={large_service_area}&format=json";
 	
 	/**
 	 * 指定したキーワードに紐づく店舗情報を取得する。
