40a41777c20a8e1b50ef9c5e358b5ca95a12c4aa
radical rogical <radical rogical@DESKTOP-RN8S4UD>
2021-07-07 13:38:14 +0900

    srdfgz

--- src/main/java/jp/ac/hcs/s3a315/user/UserController.java
+++ src/main/java/jp/ac/hcs/s3a315/user/UserController.java
@@ -1,5 +1,37 @@
 package jp.ac.hcs.s3a315.user;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
 public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	/**
+	 * 登録してるユーザ一覧を取得する。
+	 * 
+	 * @param principal ログイン情報
+	 * @param model
+	 * @return 検索結果-タスク情報
+	 */
+	
+	@GetMapping("/user/list")
+	public String getUser(Principal principal,Model model) {
+		UserEntity userEntity = userService.getTask();
+		
+		model.addAttribute("userEntity",userEntity);
+		
+		return "user/userList";
+	}
+	
 
 }
--- src/main/java/jp/ac/hcs/s3a315/user/UserService.java
+++ src/main/java/jp/ac/hcs/s3a315/user/UserService.java
@@ -1,5 +1,38 @@
 package jp.ac.hcs.s3a315.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * ユーザ情報を配列に入れる操作を行う。
+
+ */
+@Transactional
+@Service
 public class UserService {
+	
+	@Autowired
+	UserRepository userRepository;
+	/**
+	 * 登録しているユーザ情報を抽出する。
+	 * @return userEntity
+	 */
+
+	public UserEntity getTask() {
+		
+		//ユーザーエンティティの作成
+		UserEntity userEntity;
+		try {
+			userEntity = userRepository.selectAll();
+			
+		}catch(DataAccessException e){
+			e.printStackTrace();
+			userEntity = null;
+		}
+		
+		return userEntity;
+	}
 
 }
