3f42dced99f92adee7da3d65588997e9345ab25b
radical rogical <radical rogical@DESKTOP-RN8S4UD>
2021-07-07 14:55:45 +0900

    途中

--- src/main/java/jp/ac/hcs/s3a315/user/UserForm.java
+++ src/main/java/jp/ac/hcs/s3a315/user/UserForm.java
@@ -1,5 +1,11 @@
 package jp.ac.hcs.s3a315.user;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+
 import lombok.Data;
 
 /**
@@ -10,13 +16,26 @@ import lombok.Data;
 
 @Data
 public class UserForm {
+	
 	/** ユーザID（メールアドレス）*/
+	@NotBlank(message = "{require_check}")
+	@Email(message = "{email_check}")
+	private String user_id;
+	
 	/** パスワード*/
+	@NotBlank(message = "{require_check}")
+	@Length(min = 4,max = 100,message= "{length_check}")
+	@Pattern(regexp = "^[a-zA-Z0-9]+$",message = "{pattern_check}")
+	private String password;
 	/** ユーザ名*/
+	@NotBlank(message = "{require_check}")
+	private String user_name;
 	/** ダークモードフラグ*/
+	@NotBlank(message = "{require_check}")
+	private boolean darkmode;
+	
 	/** 権限*/
-	@NotBlank(message= "{require_check}");
+	@NotBlank(message = "{require_check}")
 	private String role;
-	}
 
 }
