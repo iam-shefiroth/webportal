b1e6210fbf8d0bf2e3de0b40e60a967053908965
radical rogical <radical rogical@DESKTOP-RN8S4UD>
2021-07-07 10:16:14 +0900

    自分用アカウント「セフィロス」

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -3,10 +3,10 @@
 DELETE FROM m_user;
 DELETE FROM task;
 
-/* ユーザマスタのデータ（ADMIN権限） PASS:pasword */
+/* ユーザマスタのデータ（ADMIN権限） PASS:paswords */
 INSERT INTO m_user (user_id, encrypted_password, user_name, darkmode, role, enabled)
 VALUES('isida@xxx.co.jp', '$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2ASFjDhe6WF4.Qji1huIPa', '石田悠介', false, 'ROLE_ADMIN', true);
-/* ユーザマスタのデータ（一般権限） PASS:pasword */
+/* ユーザマスタのデータ（一般権限） PASS:paswords */
 INSERT INTO m_user (user_id, encrypted_password, user_name, darkmode, role, enabled)
 VALUES('abe@xxx.co.jp', '$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2ASFjDhe6WF4.Qji1huIPa', '安部華奈', false, 'ROLE_GENERAL', true);
 INSERT INTO m_user (user_id, encrypted_password, user_name, darkmode, role, enabled)
@@ -20,4 +20,4 @@ INSERT INTO task (id, user_id, priority, title, comment, limitday) VALUES (2, 'a
 INSERT INTO task (id, user_id, priority, title, comment, limitday) VALUES (3, 'isida@xxx.co.jp', 'LOW','c', 'それやる', '2020-03-31');
 INSERT INTO task (id, user_id, priority, title, comment, limitday) VALUES (4, 'sano@xxx.co.jp', 'LOW','d', 'どれやる', '2020-03-25');
 INSERT INTO task (id, user_id, priority, title, comment, limitday) VALUES (5, 'abe@xxx.co.jp', 'LOW','e', 'もっとやる', '2020-04-20');
-INSERT INTO task (id, user_id, priority, title, comment, limitday) VALUES (6, 'user', 'MIDDLE','f', 'セフィロスごっこをする', '2022-07-22');
+INSERT INTO task (id, user_id, priority, title, comment, limitday) VALUES (6, 'serious@xxx.co.jp', 'MIDDLE','f', 'セフィロスごっこをする', '2022-07-22');
